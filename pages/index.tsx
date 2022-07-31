2e2a7aa8d1687152e42187bd911718996b057a21
Mac <liutr2014@gmail.com>
2022-07-30 20:36:47 -0700

    format

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <h1 className="text-3xl text-red-700">
-      Hello world!
-    </h1>
-  )
-}
+  return <h1 className="text-3xl text-red-700">Hello world!</h1>;
+};
 
-export default Home
+export default Home;
